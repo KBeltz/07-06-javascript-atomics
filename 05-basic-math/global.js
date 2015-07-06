a6ceacbf264c4e8ee92848310976bce89f44da43
Kristin Beltz <beltz08@me.com>
2015-07-06 18:25:53 -0500

    Small progress.

--- 05-basic-math/global.js
+++ 05-basic-math/global.js
@@ -1,17 +1,26 @@
 // Prompt the user for three numbers (make sure to parse them as integers, and make sure to store each in a well-named variable)
-var x = prompt("Please enter the first number:")
-var y = prompt("Please enter the second number:")
-var z = prompt("Please enter the third number:")
+var x = parseInt(prompt("Please enter the first number:"), 10);
+var y = parseInt(prompt("Please enter the second number:"), 10);
+var z = parseInt(prompt("Please enter the third number:"), 10);
 
 // Output the sum of the three numbers in a string: 7 + 4 + 12 = 23
-
+alert(x+y+z);
 
 // Output the difference when the second and third numbers are subtracted from the first: 7 - 4 - 12 = -9
+alert(x-y-z);
+
 // Output the product of the three numbers: 7 * 4 * 12 = 336
+alert(x*y*z);
+
 // Output the quotient of the first number divided by the second number: 7 / 4 = 1.75
+alert(x/y);
+
 // Output the result of incrementing the first number: 7++ = 8
+alert(x++);
+
 // Output the result of decrementing the second number: 4-- = 3
-//
+alert(y--);
+
 // Bonus Challenges
 //
 // Learn about the compound assignment operators (like += and -=) -- what problem do these solve and how are they used? What are situations where you might use a compound assignment operator instead of simple assignment (with =)? Which is easier to read: x += 2; or x = x + 2;?
