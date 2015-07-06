d3b2ab66d301db2fe38ebaf1fbf7d0bfcb0ad32c
Kristin Beltz <beltz08@me.com>
2015-07-06 11:23:40 -0500

    Second atomic.

---
+++ 02-output-with-alert-and-log/global.js
@@ -0,0 +1,9 @@
+alert("Hello! My name is Kikki. Welcome!")
+
+console.log("My favorite breakfast is chicken fried seitan and waffles.")
+console.log("My family includes one cat named Lord Vader.")
+console.log("My favorite place to eat lots of food is at home.")
+console.log("I currently take 0-1 naps per week.")
+console.log("I would like to take 7 naps per week.")
+
+alert("Goodbye!")
