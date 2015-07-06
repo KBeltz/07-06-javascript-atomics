ddc95f25095b59f703f0c8c175d91f2faa6b9e31
Kristin Beltz <beltz08@me.com>
2015-07-06 11:35:59 -0500

    Documentation.

--- 02-output-with-alert-and-log/global.js
+++ 02-output-with-alert-and-log/global.js
@@ -1,9 +1,14 @@
-alert("Hello! My name is Kikki. Welcome!")
+// JS Atomic 02, The Q&A Session
 
-console.log("My favorite breakfast is chicken fried seitan and waffles.")
-console.log("My family includes one cat named Lord Vader.")
-console.log("My favorite place to eat lots of food is at home.")
-console.log("I currently take 0-1 naps per week.")
-console.log("I would like to take 7 naps per week.")
+// Greeting
+alert("Hello! My name is Kikki. Welcome!");
 
-alert("Goodbye!")
+// console.log
+console.log("My favorite breakfast is chicken fried seitan and waffles.");
+console.log("My family includes one cat named Lord Vader.");
+console.log("My favorite place to eat lots of food is at home.");
+console.log("I currently take 0-1 naps per week.");
+console.log("I would like to take 7 naps per week.");
+
+// Goodbye
+alert("Goodbye!");
