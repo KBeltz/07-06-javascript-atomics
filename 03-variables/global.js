4ce36c08dc06ea8a0976984ecdb3d716fd492e49
Kristin Beltz <beltz08@me.com>
2015-07-06 13:13:54 -0500

    Using variables in javascript.

---
+++ 03-variables/global.js
@@ -0,0 +1,12 @@
+// Create three variables: name, age, and hairColor
+var name = "Kikki";
+var age = 53;
+var hairColor = "Atomic Turquoise";
+
+// Do three alert() statements, with output formatted like so:
+// My name is Andrew.
+// My age is 29.
+// My hair color is Bright Pink.
+alert("My name is " + name + ".");
+alert("My age is " + age + ".");
+alert("My hair color is " + hairColor + ".");
