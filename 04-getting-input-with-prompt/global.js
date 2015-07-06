0af4cea3368a08bb361f285d0aaa73b1af8b4f41
Kristin Beltz <beltz08@me.com>
2015-07-06 13:31:47 -0500

    Getting input from user.

---
+++ 04-getting-input-with-prompt/global.js
@@ -0,0 +1,17 @@
+//Using prompt()
+
+//getting input
+var name = prompt("What is your name?")
+var age = prompt("What is your age?")
+var hometown = prompt("What is your hometown?")
+var fav_num = prompt("What is your favorite number?")
+
+//output
+alert("Your name is " + name + ".")
+console.log("Your name is " + name + ".")
+alert("Your age is " + age + ".")
+console.log("Your age is " + age + ".")
+alert("Your hometown is " + hometown + ".")
+console.log("Your hometown is " + hometown + ".")
+alert("Your favorite number is " + fav_num + ".")
+console.log("Your favorite number is " + fav_num + ".")
