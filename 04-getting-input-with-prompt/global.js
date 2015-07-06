01522c7402d5f22f3fc8c921d9edb63232832976
Kristin Beltz <beltz08@me.com>
2015-07-06 13:58:52 -0500

    Still need to figure out console.log(typof) error

--- 04-getting-input-with-prompt/global.js
+++ 04-getting-input-with-prompt/global.js
@@ -1,17 +1,19 @@
 //Using prompt()
 
 //getting input
-var name = prompt("What is your name?")
-var age = prompt("What is your age?")
-var hometown = prompt("What is your hometown?")
-var fav_num = prompt("What is your favorite number?")
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+var hometown = prompt("What is your hometown?");
+var fav_num = prompt("What is your favorite number?");
+
+console.log(typof (age));
 
 //output
-alert("Your name is " + name + ".")
-console.log("Your name is " + name + ".")
-alert("Your age is " + age + ".")
-console.log("Your age is " + age + ".")
-alert("Your hometown is " + hometown + ".")
-console.log("Your hometown is " + hometown + ".")
-alert("Your favorite number is " + fav_num + ".")
-console.log("Your favorite number is " + fav_num + ".")
+alert("Your name is " + name + ".");
+console.log("Your name is " + name + ".");
+alert("Your age is " + age + ".");
+console.log("Your age is " + age + ".");
+alert("Your hometown is " + hometown + ".");
+console.log("Your hometown is " + hometown + ".");
+alert("Your favorite number is " + fav_num + ".");
+console.log("Your favorite number is " + fav_num + ".");
---
+++ 05-basic-math/global.js
@@ -0,0 +1,18 @@
+// Prompt the user for three numbers (make sure to parse them as integers, and make sure to store each in a well-named variable)
+x = prompt("Please enter the first number:")
+y = prompt("Please enter the second number:")
+z = prompt("Please enter the third number:")
+
+// Output the sum of the three numbers in a string: 7 + 4 + 12 = 23
+
+
+// Output the difference when the second and third numbers are subtracted from the first: 7 - 4 - 12 = -9
+// Output the product of the three numbers: 7 * 4 * 12 = 336
+// Output the quotient of the first number divided by the second number: 7 / 4 = 1.75
+// Output the result of incrementing the first number: 7++ = 8
+// Output the result of decrementing the second number: 4-- = 3
+//
+// Bonus Challenges
+//
+// Learn about the compound assignment operators (like += and -=) -- what problem do these solve and how are they used? What are situations where you might use a compound assignment operator instead of simple assignment (with =)? Which is easier to read: x += 2; or x = x + 2;?
+// Research the Javascript Math Object -- can you get some of these methods to work? I'll recommend Math.pow(), Math.max(), and Math.abs() as starting points.
