2da9387fe15b80a5d9b3713c151bc7902cccce15
Kristin Beltz <beltz08@me.com>
2015-07-07 09:40:18 -0500

    7g complete.

---
+++ 07-basic-loops/g/global.js
@@ -0,0 +1,12 @@
+// Implement the 99 Bottles of Beer on the Wall song using a loop. Make sure to account for having 1 bottle (instead of "1 bottles"). Use a for loop.
+
+
+for (var drink = parseInt(prompt("How many bottles?")); drink >= 1; drink--) {
+  if (drink === 1) {
+    console.log(drink + " bottle of beer on the wall!");
+    alert("The end!");
+  }
+  else {
+    console.log(drink + " bottles of beer on the wall!");
+  }
+}
