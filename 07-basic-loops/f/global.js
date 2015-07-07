23b6f3049035927b41974dfbd6d856ea646edac3
Kristin Beltz <beltz08@me.com>
2015-07-07 09:36:41 -0500

    7f complete.

---
+++ 07-basic-loops/f/global.js
@@ -0,0 +1,18 @@
+// Prompt the user for a start value and an end value. Count from the start value to the end value using a for loop. Make sure to account for the start-value being equal to or greater than the end-value (should it not count? Should it count down instead of up in such cases?).
+//
+// Extend this by asking the user what increment they want to count by.
+
+var start = parseInt(prompt("Enter the starting value: "));
+var end = parseInt(prompt("Enter the end value: "));
+var increment = parseInt(prompt("Enter the increment: "));
+
+if (start < end) {
+  for (; start <= end;  start = start + increment) {
+    alert(start);
+  }
+}
+else if (start > end) {
+  for (; start >= end;  start = start - increment) {
+    alert(start);
+  }
+}
